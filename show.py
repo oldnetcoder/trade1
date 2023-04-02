a60956c2159a262f6d1f797bf2b85f29ab5495a3
oldnetcoder <86482477+oldnetcoder@users.noreply.github.com>
2023-04-02 12:56:30 +0000

    commit

--- show.py
+++ show.py
@@ -10,11 +10,5 @@ def index():
     c.execute('SELECT * FROM trades order by id desc')
     trades = c.fetchall()
     conn.close()
-    trades_with_profit = []
-    for trade in trades:
-        if trade[3] is not None:
-            profit_rate = round((trade[3] - trade[2]) / trade[2] * 100, 2)
-            trades_with_profit.append((trade[0], trade[1], trade[2], trade[3], trade[4], trade[5], profit_rate))
-        else:
-            trades_with_profit.append((trade[0], trade[1], trade[2], trade[3], trade[4], trade[5], 'N/A'))
-    return render_template('index.html', trades=trades_with_profit)
+    
+    return render_template('index.html', rows=trades)
